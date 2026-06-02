1b10462d43471d235a3417b0e1db7cad31c9f8f6
NicholasC2 <nicholasc2@proton.me>
2026-06-03 09:52:02 +1200

    added colors and fixed stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,36 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import placeholderImage from './assets/placeholder.jpeg'
 import './index.css'
-import App from './App.jsx'
+
+import './colors.css'
+import './global.css'
+
+function App() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <button
+        className="menuButton"
+        onClick={() => setOpen(!open)}
+      >
+        ☰
+      </button>
+      <div className={`navbar ${open ? 'open' : ''}`}>
+        <label>Search:</label>
+        <div className="row search">
+          <input />
+          <button>Search</button>
+        </div>
+        <hr></hr>
+      </div>
+      <div className="content">
+
+      </div>
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
